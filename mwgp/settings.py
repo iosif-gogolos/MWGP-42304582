762913ce43761b4f04df4fc555ff2eaaa433e4e9
iosif-gogolos <iosif.gogolos@icloud.com>
2025-05-24 12:10:37 +0200

    register models in admin

--- mwgp/settings.py
+++ mwgp/settings.py
@@ -38,9 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
-    "reinigung",
-    "kalender",
-   
+    "reinigung",   
 ]
 
 MIDDLEWARE = [
--- reinigung/admin.py
+++ reinigung/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import WG, MitbewohnerProfil, Raum, Reinigungsaufgabe, Putzplan, Kalendereintrag, AufgabenStatus
+
 # Register your models here.
+
+admin.site.register(WG)
+admin.site.register(MitbewohnerProfil)
+admin.site.register(Raum)
+admin.site.register(Reinigungsaufgabe)
+admin.site.register(Putzplan)
+admin.site.register(AufgabenStatus)
+admin.site.register(Kalendereintrag)
+
+
